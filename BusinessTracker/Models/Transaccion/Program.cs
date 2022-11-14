dc12be4197ccdfae9d40d6d56f863cc0032b8328
Danny <dannymou7426@hotmail.com>
2022-11-13 23:32:11 -0600

    Mensaje

---
+++ BusinessTracker/Models/Transaccion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessTracker.Models
+{
+    public class Transaccion
+    {
+        [Key]
+        public int TransactionId {get; set;}
+
+        [(TypeName = "nvchar(50)")]
+
+        public string NameId {get; set;}
+
+        [(TypeName = "nvachar(50)")]
+
+        public Comida Name {get; set;}
+
+        [(TypeName = "nvarchar(50)")]
+
+        public double Amount {get; set;}
+
+        [(TypeName = "nvachar(50)")]
+
+        public string Description {get; set;}
+
+        [(TypeName = "nvachar(10)")]
+
+        public DateTime Date {get; set;} = DateTime.Now;
+    }
+}
