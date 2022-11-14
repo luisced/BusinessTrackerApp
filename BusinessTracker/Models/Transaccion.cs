3d559c5dd4056d4975f0374a0515ccf983c027dd
luisced <luis.cedillo.maldonado@gmail.com>
2022-11-14 01:51:09 -0600

    Docer

---
+++ BusinessTracker/Models/Transaccion.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessTracker.Models
+{
+    public class Transaccion
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+
+        public string NameId { get; set; } = "N/A";
+
+        [Column(TypeName = "nvarchar(50)")]
+
+        public Comida Name { get; set; } = new Comida();
+
+        [Column(TypeName = "nvarchar(50)")]
+
+        public double Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+
+        public string Description { get; set; } = "N/A";
+
+        [Column(TypeName = "nvarchar(10)")]
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
